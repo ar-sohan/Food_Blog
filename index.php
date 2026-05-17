92a69d6324faddec9877867ab54b0245388ef8fd
ar-sohan <arsohan101@gmail.com>
2026-05-17 13:02:07 +0600

    Redesigned the project structure with initial home page

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
-<?php include './views/components/header.php' ?>
-<?php include './views/components/navbar.php' ?>
-<?php include './views/components/footer.php' ?>
+<?php
+
+    session_start();
+    if(!isset($_SESSION['user_id']) && isset($_COOKIE['remember_token'])){
+        require_once('model/userModel.php');
+        $u = findUserByRememberToken($_COOKIE['remember_token']);
+        if($u){
+            $_SESSION['user_id'] = $u['id'];
+            $_SESSION['name']    = $u['name'];
+            $_SESSION['role']    = $u['role'];
+        }
+    }
+
+    header('location: view/home.php');
+
+?>
